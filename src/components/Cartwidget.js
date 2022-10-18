713204308e62608428101e711a0ea6a479b559c8
fedegeretto <fede.geretto96@gmail.com>
2022-10-18 16:02:46 -0300

    Terminar navbar y tp1

---
+++ src/components/Cartwidget.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cartwidget = () => {
+  return (
+    <div>Cartwidget</div>
+  )
+}
+
+export default Cartwidget
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <h1>FAG TIENDA FUTBOLERA</h1>
+  )
+}
+
+export default Header
